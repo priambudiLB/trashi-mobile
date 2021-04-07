0106431452f55361f6944c875de8dc982e529d0a
hanifagung8 <hanif.agung@gmail.com>
2021-04-07 22:57:49 +0700

    create Latest News text

--- lib/pages/home_screen/home_screen.dart
+++ lib/pages/home_screen/home_screen.dart
@@ -5,6 +5,8 @@ import 'package:trashi/constants/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String PATH = "home";
+  final String latestNewsText = "Latest News";
+  final double latestNewsFontSize = 30;
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -16,7 +18,18 @@ class _HomeScreenState extends State<HomeScreen> {
     final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: ListView(
-        children: <Widget>[NewsSearchBar()],
+        children: <Widget>[
+          NewsSearchBar(),
+          Text(
+            widget.latestNewsText,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: "Poppins",
+              fontWeight: FontWeight.w800,
+              fontSize: widget.latestNewsFontSize,
+            ),
+          )
+        ],
       ),
       backgroundColor: hexToColor(MAIN_COLOR),
     );
