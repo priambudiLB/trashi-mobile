d6192fda6578a7e7e0bbb02efb31921475b8586a
indraseptama <indraseptama28@gmail.com>
2021-04-10 14:52:07 +0700

    add color string

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,6 +1,8 @@
 // Store all color codes from Figma here
 // Format: #<color hex>
 const String MAIN_COLOR = "#509761";
+const String COLOR_YELLOW = "#FFC845";
+const String COLOR_RED = "#D8224D";
 const String COLOR_WHITE = "#FFFFFF";
 const String COLOR_BLACK = "#000000";
 const String NAVBAR_COLOR = "#EEEFEF";
@@ -9,5 +11,6 @@ const String COLOR_RED_NEGATIVE = "#F14363";
 const String COLOR_MOOD_YELLOW = "#FFA843";
 const String COLOR_MOOD_PINK = "#FD88A4";
 const String COLOR_INACTIVE_GREY = "#C4C4C4";
+
 
 // TODO: Store more color codes here
