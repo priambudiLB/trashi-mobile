a97dddf9793481e39ea93530f61b8cd46998eb80
Hanif Agung <hanif.agung@gmail.com>
2021-05-02 15:11:49 +0700

    start using Layout

--- lib/pages/registration_screen/account_type_selection_screen.dart
+++ lib/pages/registration_screen/account_type_selection_screen.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:trashi/components/layout_redesign.dart';
 
 class AccountTypeSelectionScreen extends StatelessWidget {
   static const String PATH = "accountTypeSelection";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-      backgroundColor: Colors.white,
-      leading: IconButton(icon: Icon(Icons.arrow_back, color: Colors.black)),
-      elevation: 0,
-    ));
+    return Layout();
   }
 }
