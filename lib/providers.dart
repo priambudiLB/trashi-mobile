d40707c4554efa0f1a7920d30588317b3a778a36
indraseptama <indraseptama28@gmail.com>
2021-05-08 17:38:31 +0700

    add provider

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,7 +1,11 @@
 // ignore_for_file: public_member_api_docs, lines_longer_than_80_chars
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:formz/formz.dart';
 import 'package:intl/intl.dart';
+import 'package:trashi/status_upload.dart';
 import 'package:trashi/verification.dart';
 
 /// Mix-in [DiagnosticableTreeMixin] to have access to [debugFillProperties] for the devtool
@@ -253,5 +257,79 @@ class OTP with ChangeNotifier, DiagnosticableTreeMixin {
   void verify() {
     //TODO
     setPopUpSuccessOpen(true);
+  }
+}
+
+class SubmitDocumentVerification with ChangeNotifier, DiagnosticableTreeMixin {
+  StatusUpload _generalStatus = StatusUpload.NOT_UPLOADED;
+  StatusUpload _ktpStatus = StatusUpload.NOT_UPLOADED;
+  StatusUpload _kkStatus = StatusUpload.NOT_UPLOADED;
+  StatusUpload _ktpAndKkStatus = StatusUpload.NOT_UPLOADED;
+  FormzStatus _statusFetchData = FormzStatus.pure;
+  File _fileKtp;
+  File _fileKk;
+  File _fileKtpAndKK;
+
+  StatusUpload get generalStatus => _generalStatus;
+  StatusUpload get ktpStatus => _ktpStatus;
+  StatusUpload get kkStatus => _kkStatus;
+  StatusUpload get ktpAndKkStatus => _ktpAndKkStatus;
+  FormzStatus get statusFetchData => _statusFetchData;
+  File get fileKtp => _fileKtp;
+  File get fileKk => _fileKk;
+  File get fileKtpAndKK => _fileKtpAndKK;
+
+  void setStatusFetchData(FormzStatus status) {
+    _statusFetchData = status;
+    notifyListeners();
+  }
+
+  void fetchData() async {
+    setStatusFetchData(FormzStatus.submissionInProgress);
+
+    Future.delayed(Duration(seconds: 1), () {
+      setGeneralStatus(StatusUpload.NOT_UPLOADED);
+      setKtpStatus(StatusUpload.FAILED);
+      setKkStatus(StatusUpload.FAILED);
+      setKtpAndKkStatus(StatusUpload.FAILED);
+    });
+    // TODO fetch data from API
+
+    setStatusFetchData(FormzStatus.submissionSuccess);
+  }
+
+  void setGeneralStatus(StatusUpload newStatus) {
+    _generalStatus = newStatus;
+    notifyListeners();
+  }
+
+  void setKtpStatus(StatusUpload newStatus) {
+    _ktpStatus = newStatus;
+    notifyListeners();
+  }
+
+  void setKkStatus(StatusUpload newStatus) {
+    _kkStatus = newStatus;
+    notifyListeners();
+  }
+
+  void setKtpAndKkStatus(StatusUpload newStatus) {
+    _ktpAndKkStatus = newStatus;
+    notifyListeners();
+  }
+
+  void setKtpFile(File file) {
+    _fileKtp = file;
+    notifyListeners();
+  }
+
+  void setKkFile(File file) {
+    _fileKk = file;
+    notifyListeners();
+  }
+
+  void setKtpAndKkFile(File file) {
+    _fileKtpAndKK = file;
+    notifyListeners();
   }
 }
