1e539a32d7e8722d7b714c3cdabea8f4337a0799
Hanif Agung <hanif.agung@gmail.com>
2021-05-19 20:59:29 +0700

    feat/create_ui_for_choose_calendar_button

--- lib/pages/trash_collection_screen/accepter/accept_trash_collection_request_screen.dart
+++ lib/pages/trash_collection_screen/accepter/accept_trash_collection_request_screen.dart
@@ -13,16 +13,20 @@ class AcceptTrashCollectionRequestScreen extends StatefulWidget {
 
 class _AcceptTrashCollectionRequestScreenState
     extends State<AcceptTrashCollectionRequestScreen> {
+  BoxDecoration _buildBoxDecoration() {
+    return BoxDecoration(
+      borderRadius: BorderRadius.all(
+        Radius.circular(8),
+      ),
+      border: Border.all(
+        color: hexToColor("#F2F2F2"),
+      ),
+    );
+  }
+
   Widget _buildSearchBar() {
     return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(
-          Radius.circular(8),
-        ),
-        border: Border.all(
-          color: hexToColor("#F2F2F2"),
-        ),
-      ),
+      decoration: _buildBoxDecoration(),
       child: TextField(
         textAlignVertical: TextAlignVertical.center,
         cursorColor: hexToColor(MAIN_COLOR),
@@ -50,6 +54,33 @@ class _AcceptTrashCollectionRequestScreenState
     );
   }
 
+  Widget _buildChooseCalendarButton() {
+    return Container(
+      decoration: _buildBoxDecoration(),
+      padding: EdgeInsets.all(14),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.calendar_today_outlined,
+            size: 14,
+          ),
+          Text(
+            "1 Januari 2021",
+            style: TextStyle(
+              color: hexToColor("#4C4C4C"),
+              fontSize: 14,
+            ),
+          ),
+          Icon(
+            Icons.keyboard_arrow_down,
+            size: 14,
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Layout(
@@ -64,6 +95,16 @@ class _AcceptTrashCollectionRequestScreenState
       body: Column(
         children: [
           _buildSearchBar(),
+          Padding(
+            padding: EdgeInsets.only(
+              bottom: 12,
+            ),
+          ),
+          Row(
+            children: [
+              _buildChooseCalendarButton(),
+            ],
+          )
         ],
       ),
     );
