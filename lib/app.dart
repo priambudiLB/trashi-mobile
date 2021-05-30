f9b98687d33156d8d83fa62bedcfbf888dd0621a
indraseptama <indraseptama28@gmail.com>
2021-05-30 22:32:52 +0700

    add request screen

--- lib/app.dart
+++ lib/app.dart
@@ -7,6 +7,7 @@ import 'package:trashi/pages/home_screen/home_screen.dart';
 import 'package:trashi/pages/onboarding_redesign_screen/on_boarding_screen_view.dart';
 import 'package:trashi/pages/registration_document_confirmation/profile_example.dart';
 import 'package:trashi/pages/registration_document_confirmation/registration_document_confirmation.dart';
+import 'package:trashi/pages/request_screen/request_screen.dart';
 import 'package:trashi/pages/retribution_screen/retribution_screen.dart';
 import 'package:trashi/pages/splash_screen/splash_screen.dart';
 import 'package:trashi/verification.dart';
@@ -19,7 +20,7 @@ class TrashiApplication extends StatelessWidget {
     return MaterialApp(
       theme: defaultTheme,
       title: APP_NAME,
-      initialRoute: FormRequestPengangkatan.PATH,
+      initialRoute: RequestScreen.PATH,
       routes: <String, WidgetBuilder>{
         // TODO: Add more page and define its path here
         SplashScreen.PATH: (_) => SplashScreen(),
@@ -32,7 +33,8 @@ class TrashiApplication extends StatelessWidget {
         ProfileExample.PATH: (_) => ProfileExample(),
         RegistrationDocumentConfirmation.PATH: (_) =>
             RegistrationDocumentConfirmation(),
-        FormRequestPengangkatan.PATH: (_) => FormRequestPengangkatan()
+        FormRequestPengangkatan.PATH: (_) => FormRequestPengangkatan(),
+        RequestScreen.PATH: (_) => RequestScreen()
         // HomeScreen.PATH: (_) => HomeScreen(),
       },
     );
---
+++ lib/pages/request_screen/request_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:trashi/components/layout_redesign.dart';
+import 'package:trashi/components/routes.dart';
+import 'package:trashi/pages/form_request_pengangkatan/provider.dart';
+import 'package:trashi/utils/commons.dart';
+import 'package:trashi/pages/form_request_pengangkatan/form_request_pengangkatan.dart';
+
+class RequestScreen extends StatelessWidget {
+  static const String PATH = "request_screen";
+  @override
+  Widget build(BuildContext context) {
+    return Layout(
+      title: Text(""),
+      body: Column(
+        children: [
+          InkWell(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            onTap: () {
+              Navigator.push(
+                  context, SlideLeftRoute(page: FormRequestPengangkatan()));
+            },
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Request Pengangkatan",
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: hexToColor("#464646"),
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Container(
+                        height: 8,
+                      ),
+                      Text(
+                        "Request pengangkatan sampah",
+                        style: TextStyle(
+                            fontSize: 12, color: hexToColor("#909090")),
+                      )
+                    ],
+                  ),
+                ),
+                Icon(
+                  Icons.chevron_right_outlined,
+                  size: 40,
+                  color: hexToColor("#4C4C4C"),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 16, bottom: 16),
+            height: 1,
+            width: double.infinity,
+            color: hexToColor("#DFDFDF"),
+          ),
+          InkWell(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            onTap: () {
+              //TODO
+            },
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Histori Pengangkatan",
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: hexToColor("#464646"),
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Container(
+                        height: 8,
+                      ),
+                      Text(
+                        "Lihat histori pengangkatan sampah",
+                        style: TextStyle(
+                            fontSize: 12, color: hexToColor("#909090")),
+                      )
+                    ],
+                  ),
+                ),
+                Icon(
+                  Icons.chevron_right_outlined,
+                  size: 40,
+                  color: hexToColor("#4C4C4C"),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 16, bottom: 16),
+            height: 1,
+            width: double.infinity,
+            color: hexToColor("#DFDFDF"),
+          ),
+        ],
+      ),
+    );
+  }
+}
