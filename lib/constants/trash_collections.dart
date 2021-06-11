de46fe03295af4be36decd5b9ff92d71876c4254
hanifagung8 <hanif.agung@gmail.com>
2021-06-11 18:42:54 +0700

    Feat/create histori pengangkatan screen (#52)
    * feat/create history card and model
    
    * feat/finish card design and config
    
    * feat/add history detail

---
+++ lib/constants/trash_collections.dart
@@ -0,0 +1,5 @@
+const String paymentStatusPaid = "Lunas";
+const String paymentStatusNotPaid = "Belum lunas";
+
+const String requestStatusWaiting = "Menunggu Pengangkatan";
+const String requestStatusFinished = "Selesai";
