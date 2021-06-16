cb8d6125ea1cadda0922d79862c5eef9850721bf
indraseptama <indraseptama28@gmail.com>
2021-06-16 22:10:41 +0700

    complete ui profile for rakyat jelata role

--- lib/pages/profile_screen_redesign/profile_screen_new.dart
+++ lib/pages/profile_screen_redesign/profile_screen_new.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:trashi/constants/colors.dart';
+import 'package:trashi/utils/commons.dart';
 
 class ProfileScreenRedesign extends StatelessWidget {
   static const String PATH = "profile_redesign";
@@ -8,144 +10,246 @@ class ProfileScreenRedesign extends StatelessWidget {
     return Scaffold(
       body: ListView(
         children: [
+          ProfileHeader(size: size),
+          ItemTileProfile(
+            icon: Icons.text_snippet_outlined,
+            ontap: () {},
+            title: "Daftar Harga Pengangkatan",
+          ),
           Container(
-            height: 300,
-            child: Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(16),
-                      bottomRight: Radius.circular(16)),
-                  child: Image.asset(
-                    "assets/images/small_green_background.png",
-                    width: size.width,
+            height: 1,
+            margin: EdgeInsets.symmetric(horizontal: 24),
+            color: hexToColor("#DFDFDF"),
+          ),
+          ItemTileProfile(
+            icon: Icons.lock_outline,
+            ontap: () {},
+            title: "Privacy",
+          ),
+          Container(
+            height: 1,
+            margin: EdgeInsets.symmetric(horizontal: 24),
+            color: hexToColor("#DFDFDF"),
+          ),
+          ItemTileProfile(
+              icon: Icons.settings_outlined, ontap: () {}, title: "Setting"),
+          Container(
+            height: 1,
+            margin: EdgeInsets.symmetric(horizontal: 24),
+            color: hexToColor("#DFDFDF"),
+          ),
+          ItemTileProfile(icon: Icons.logout, ontap: () {}, title: "Logout"),
+        ],
+      ),
+    );
+  }
+}
+
+class ItemTileProfile extends StatelessWidget {
+  const ItemTileProfile({
+    Key key,
+    @required this.icon,
+    @required this.ontap,
+    @required this.title,
+  }) : super(key: key);
+
+  final IconData icon;
+  final Function ontap;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: ontap,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 20,
+              color: hexToColor(MAIN_COLOR),
+            ),
+            Container(
+              width: 18,
+            ),
+            Expanded(
+              child: Text(
+                title,
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Icon(Icons.arrow_forward_ios_outlined)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileHeader extends StatelessWidget {
+  const ProfileHeader({
+    Key key,
+    @required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(16),
+                bottomRight: Radius.circular(16)),
+            child: Image.asset(
+              "assets/images/small_green_background.png",
+              width: size.width,
+            ),
+          ),
+          Positioned(
+              left: 24,
+              top: 58,
+              child: Text(
+                "Hello,",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 24),
+              )),
+          Positioned(
+              left: 24,
+              top: 95,
+              child: Text(
+                "Welcome to Trashi",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 14),
+              )),
+          Positioned(
+              top: 67,
+              right: 24,
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.notifications_none,
+                    size: 32.0,
+                    color: Colors.white,
                   ),
-                ),
-                Positioned(
-                    left: 24,
-                    top: 58,
-                    child: Text(
-                      "Hello,",
-                      style: TextStyle(
+                  Container(
+                    width: 25,
+                  ),
+                  ClipOval(
+                      child: Container(
+                          height: 30,
+                          width: 30,
                           color: Colors.white,
-                          fontWeight: FontWeight.w800,
-                          fontSize: 24),
-                    )),
-                Positioned(
-                    left: 24,
-                    top: 95,
-                    child: Text(
-                      "Welcome to Trashi",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 14),
-                    )),
-                Positioned(
-                    top: 67,
-                    right: 24,
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.notifications_none,
-                          size: 26.0,
-                          color: Colors.white,
-                        ),
-                        Container(
-                          width: 25,
-                        ),
-                        Container(
-                          height: 32,
-                          width: 32,
-                          color: Colors.blue,
-                        ),
-                      ],
-                    )),
-                Positioned(
-                    top: 144,
-                    child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 24),
-                      width: size.width,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          child:
+                              Image.asset("assets/images/Icon_success.png"))),
+                ],
+              )),
+          Positioned(
+              top: 160,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                width: size.width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 115,
+                      width: 148,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.08),
+                            blurRadius: 7,
+                            offset: Offset(0, 4), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Container(
-                            height: 115,
-                            width: 148,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(8),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.08),
-                                  blurRadius: 7,
-                                  offset: Offset(
-                                      0, 4), // changes position of shadow
+                          ClipOval(
+                            child: Container(
+                              padding: EdgeInsets.all(6),
+                              height: 32,
+                              width: 32,
+                              color: hexToColor(MAIN_COLOR),
+                              child: Center(
+                                child: Image.asset(
+                                  "assets/images/wallet_icon.png",
+                                  width: 15,
+                                  height: 15,
                                 ),
-                              ],
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Container(
-                                  height: 32,
-                                  width: 32,
-                                  color: Colors.blue,
-                                ),
-                                Container(
-                                  height: 12,
-                                ),
-                                Text(
-                                  "Pembayaran\nRetribusi",
-                                  textAlign: TextAlign.center,
-                                )
-                              ],
+                              ),
                             ),
                           ),
                           Container(
-                              height: 115,
-                              width: 148,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(8),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black.withOpacity(0.08),
-                                    blurRadius: 7,
-                                    offset: Offset(
-                                        0, 4), // changes position of shadow
-                                  ),
-                                ],
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    height: 32,
-                                    width: 32,
-                                    color: Colors.blue,
-                                  ),
-                                  Container(
-                                    height: 12,
-                                  ),
-                                  Text(
-                                    "Pembayaran\nRetribusi",
-                                    textAlign: TextAlign.center,
-                                  )
-                                ],
-                              )),
+                            height: 12,
+                          ),
+                          Text(
+                            "Pembayaran\nRetribusi",
+                            textAlign: TextAlign.center,
+                          )
                         ],
                       ),
-                    ))
-              ],
-            ),
-          ),
-          Container(
-            height: 100,
-            color: Colors.black,
-          )
+                    ),
+                    Container(
+                        height: 115,
+                        width: 148,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.08),
+                              blurRadius: 7,
+                              offset:
+                                  Offset(0, 4), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            ClipOval(
+                              child: Container(
+                                padding: EdgeInsets.all(6),
+                                height: 32,
+                                width: 32,
+                                color: hexToColor(MAIN_COLOR),
+                                child: Center(
+                                  child: Image.asset(
+                                    "assets/images/bag_icon.png",
+                                    width: 15,
+                                    height: 15,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              height: 12,
+                            ),
+                            Text(
+                              "Pembayaran\nRetribusi",
+                              textAlign: TextAlign.center,
+                            )
+                          ],
+                        )),
+                  ],
+                ),
+              ))
         ],
       ),
     );
