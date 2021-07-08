91727624e2650091edb34c98f61be47f7ca9263c
indraseptama <indraseptama28@gmail.com>
2021-07-08 21:15:01 +0700

    feat: add primary color to picker

--- lib/constants/theme.dart
+++ lib/constants/theme.dart
@@ -3,6 +3,7 @@ import 'package:trashi/constants/colors.dart';
 import 'package:trashi/utils/commons.dart';
 
 final ThemeData modiAppDefaultTheme = ThemeData(
+  colorScheme: ColorScheme.light().copyWith(primary: hexToColor(MAIN_COLOR)),
   brightness: Brightness.light,
   fontFamily: 'Avenir',
   primaryColor: hexToColor(MAIN_COLOR),
